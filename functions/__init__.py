a02179ffa0ac031f5b958c2ad805e352b7546c5e
MPins <pinsdorf@proton.me>
2025-08-29 17:38:17 -0700

    functions: deleting adressess.py bip_0340_reference.py bip_350_bech32_reference.py musig.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,7 +1,5 @@
-from functions.addresses import *
 # from functions.create_regtest_utxo import *
 from functions.setup_testshell import *
 from functions.hashfunctions import *
 from functions.script import *
-import functions.bip_0340_reference as tr
 from functions.wallet import *
